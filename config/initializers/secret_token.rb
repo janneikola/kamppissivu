0337c5f39f3e1ae96b4905f0bba11cfce4a4e94b
Janne Ikola <janne@orangead.fi>
2011-08-26 11:40:32 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kamppis::Application.config.secret_token = '3159e3b60337cc39e9f8ff58692ea522762e18149c59fdc5a775250293fd68e7eb7f58cea0a89a15d07ef02e4813640998d83bfd91576edfb11d79f650e78935'
